5974f4ceb42226123a1c639298e2c2e38dd4789d
Ivan Lopes <lopesivan.ufrj@gmail.com>
2026-06-16 19:15:08 -0300

    :warning:  Arquivo loja-python/src/loja/__main__.py marcado para deleção

--- loja-python/src/loja/__main__.py
+++
@@ -1,9 +0,0 @@
-from loja import Produto, Carrinho
-
-carrinho = Carrinho()
-
-carrinho.adicionar(Produto("Livro", 50.0))
-carrinho.adicionar(Produto("Caneta", 5.0))
-
-print(f"Quantidade: {carrinho.quantidade()}")
-print(f"Total: {carrinho.total()}")
