0734aec72551cd658a97b4fe6daa705b259781ea
Ivan Lopes <lopesivan.ufrj@gmail.com>
2026-06-16 20:15:19 -0300

    :add: Adiciona o arquivo loja-java/src/main/java/br/eng/ivanlopes/loja/Carrinho.java

---
+++ loja-java/src/main/java/br/eng/ivanlopes/loja/Carrinho.java
@@ -0,0 +1,26 @@
+package br.eng.ivanlopes.loja;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Carrinho {
+    private final List<Produto> produtos = new ArrayList<>();
+
+    public void adicionar(Produto produto) {
+        produtos.add(produto);
+    }
+
+    public int quantidade() {
+        return produtos.size();
+    }
+
+    public double total() {
+        return produtos.stream()
+                .mapToDouble(Produto::preco)
+                .sum();
+    }
+
+    public List<Produto> produtos() {
+        return List.copyOf(produtos);
+    }
+}
