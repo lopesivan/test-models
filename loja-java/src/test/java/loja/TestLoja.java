044b80edb9814229bd135a7838a085a6bbf25fce
Ivan Lopes <lopesivan.ufrj@gmail.com>
2026-06-16 19:49:05 -0300

    :style: Atualiza o estilo do arquivo loja-java/src/test/java/loja/TestLoja.java

--- loja-java/src/test/java/loja/TestLoja.java
+++ loja-java/src/test/java/loja/TestLoja.java
@@ -8,6 +8,7 @@ public class TestLoja {
 
     @Test
     void carrinhoDeveSomarProdutos() {
+
         Carrinho carrinho = new Carrinho();
 
         carrinho.adicionar(new Produto("Livro", 50.0));
@@ -15,5 +16,14 @@ public class TestLoja {
 
         assertEquals(2, carrinho.quantidade());
         assertEquals(55.0, carrinho.total());
+    }
+
+    @Test
+    void produtoDeveConterNomeEPreco() {
+
+        Produto produto = new Produto("Livro", 50.0);
+
+        assertEquals("Livro", produto.nome());
+        assertEquals(50.0, produto.preco());
     }
 }
