db5b23663ecdcc7f3fa5b680546f267a42fa7f57
Ivan Lopes <lopesivan.ufrj@gmail.com>
2026-06-16 19:16:38 -0300

    :warning:  Arquivo loja-ruby/main.rb marcado para deleção

--- loja-ruby/main.rb
+++
@@ -1,9 +0,0 @@
-require_relative "lib/loja/produto"
-require_relative "lib/loja/carrinho"
-
-carrinho = Loja::Carrinho.new
-carrinho.adicionar(Loja::Produto.new("Livro", 50.0))
-carrinho.adicionar(Loja::Produto.new("Caneta", 5.0))
-
-puts "Quantidade: #{carrinho.quantidade}"
-puts "Total: R$ #{carrinho.total}"
