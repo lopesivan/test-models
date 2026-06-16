54fa0a7dc0e6257499bd993ef97ef917b69bfab1
Ivan Lopes <lopesivan.ufrj@gmail.com>
2026-06-16 17:21:36 -0300

    :style: Atualiza o estilo do arquivo loja-cpp-catch2/tests/test_carrinho.cpp

--- loja-cpp-catch2/tests/test_carrinho.cpp
+++ loja-cpp-catch2/tests/test_carrinho.cpp
@@ -1,17 +1,15 @@
+#include <catch2/catch_test_macros.hpp>
+
 #include "produto.hpp"
 #include "carrinho.hpp"
 
-#include <cassert>
-
-int main()
+TEST_CASE("Carrinho soma produtos")
 {
     Carrinho carrinho;
 
     carrinho.adicionar(Produto("Livro", 50.0));
     carrinho.adicionar(Produto("Caneta", 5.0));
 
-    assert(carrinho.quantidade() == 2);
-    assert(carrinho.total() == 55.0);
-
-    return 0;
+    REQUIRE(carrinho.quantidade() == 2);
+    REQUIRE(carrinho.total() == 55.0);
 }
