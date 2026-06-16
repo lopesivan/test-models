67cd2e2fd37a7390a12a1d1b431ae84ed1f987e2
Ivan Lopes <lopesivan.ufrj@gmail.com>
2026-06-16 19:49:05 -0300

    :style: Atualiza o estilo do arquivo loja-java/src/main/java/loja/Carrinho.java

--- loja-java/src/main/java/loja/Carrinho.java
+++ loja-java/src/main/java/loja/Carrinho.java
@@ -4,21 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Carrinho {
+
     private final List<Produto> produtos = new ArrayList<>();
 
     public void adicionar(Produto produto) {
         produtos.add(produto);
     }
 
-    public double total() {
-        double soma = 0.0;
-        for (Produto p : produtos) {
-            soma += p.getPreco();
-        }
-        return soma;
-    }
-
     public int quantidade() {
         return produtos.size();
+    }
+
+    public double total() {
+        return produtos.stream()
+                       .mapToDouble(Produto::preco)
+                       .sum();
     }
 }
