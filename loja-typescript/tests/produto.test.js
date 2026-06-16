e5f967d2f30b3ba93df6274c41cc5a6a54982680
Ivan Lopes <lopesivan.ufrj@gmail.com>
2026-06-16 19:15:08 -0300

    :warning:  Arquivo loja-typescript/tests/produto.test.js marcado para deleção

--- loja-typescript/tests/produto.test.js
+++
@@ -1,9 +0,0 @@
-const assert = require("node:assert");
-const test = require("node:test");
-
-test("produto tem nome e preço", () => {
-  const produto = { nome: "Livro", preco: 50.0 };
-
-  assert.strictEqual(produto.nome, "Livro");
-  assert.strictEqual(produto.preco, 50.0);
-});
